5f531defb0a1f158244f73199e8e072c02557910
deepikagandham <deepikagandham5@gmail.com>
2023-03-23 23:22:59 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hema(request):
+    return render(request,'hema.html')
+
+def prema(request):
+    return render(request,'prema.html')
